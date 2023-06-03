772e548e554377dd0e2be43525636ac1f2e0535a
jacksoncardosoleite <cardosoleite99@gmail.com>
2023-06-03 20:33:03 -0300

    feat: cria aplicacao de cronometro

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import {Home} from './src/screens/Home';
+import { ThemeProvider } from 'styled-components'
+import colors from './src/styles/colors';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={colors}  >
+      <Home/>
+      <StatusBar style="dark" />
+    </ThemeProvider>
+  );
+}
+
